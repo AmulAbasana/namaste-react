2a4694313485cac5254825cdfaa34f6ffa069c16
Amul Abasana <amulabasana@gmail.com>
2023-09-16 15:47:03 +0530

    EP.5 Part 02

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,36 @@
+import { useState } from "react";
 import { RESTAURANT_LIST } from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
+  const [restaurantList, setRestaurantList] = useState(RESTAURANT_LIST);
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter-btns">
+        <button
+          className="top-rated-btn"
+          onClick={() => {
+            const filteredList = RESTAURANT_LIST.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setRestaurantList(filteredList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+        {restaurantList !== RESTAURANT_LIST && (
+          <button
+            className="clear-btn"
+            onClick={() => {
+              setRestaurantList(RESTAURANT_LIST);
+            }}
+          >
+            Clear
+          </button>
+        )}
+      </div>
       <div className="retro-container">
-        {RESTAURANT_LIST.map((restaurant) => (
+        {restaurantList.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
